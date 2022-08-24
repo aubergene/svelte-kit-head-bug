4345d0539abeffd6ecf881555bddc6445d139f20
Julian Burgess <jburgess29@bloomberg.net>
2022-08-24 13:57:27 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Meta from './Meta.svelte';
+</script>
+
+<nav>
+	<ul>
+		<li><a href="/">home</a></li>
+		<li><a href="/woo">woo</a></li>
+		<li><a href="/blah">blah</a></li>
+	</ul>
+</nav>
+
+<p>
+	When you client-side navigate between pages you will get a second <code>&lt;title&gt;</code> tag appended
+</p>
+
+<svelte:head>
+	<Meta />
+</svelte:head>
+
+<slot />
---
+++ src/routes/Meta.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+
+	let title;
+	let description;
+
+	$: {
+		$page;
+		title = Math.random();
+		description = Math.random();
+	}
+</script>
+
+<title>{title}</title>
+<meta name="description" content={description} />
